3ee2b4cdecb7f9574887dd3a5d259df573d38fda
Claudio Matos <claudiomatos@gmail.com>
2024-04-14 22:20:29 -0300

    Configurações de deploy

--- src/index.js
+++ src/index.js
@@ -2,13 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { defineConfig } from './pages/Home/index.js';
-// import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/primeflix/',
-});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
