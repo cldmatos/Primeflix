9e6cb21c8bc434fc52be7ede6fb590194034cce7
Claudio Matos <claudiomatos@gmail.com>
2024-04-14 21:29:15 -0300

    Configurações de deploy

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { defineConfig } from '.pages/Home/index.js';
+import { defineConfig } from './pages/Home/index.js';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
